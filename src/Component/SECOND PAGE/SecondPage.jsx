5141f0396b2636fa436008fc7d0e3de39f18b6e3
Shubhjeet Chakraborty <118995345+codexshub@users.noreply.github.com>
2024-05-05 01:04:59 +0530

    SecondPage.jsx

--- src/Component/SECOND PAGE/SecondPage.jsx
+++ src/Component/SECOND PAGE/SecondPage.jsx
@@ -7,20 +7,20 @@ const SecondPage = () => {
     <section className='secondPage-section'>
       <div className="container">
       <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 justify-content-center align-items-center">
-        <div className="secondPage-Heading col-12 col-sm-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6 text-center justify-content-center">
-          Get Possibilities for Better Trading with <span >POWER<span className='span1' data-aos="fade-left">TECH</span></span>
+        <div className="secondPage-Heading col-12 col-sm-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
+          <div>Get Possibilities for Better Trading with <span >POWER<span className='span1' data-aos="fade-left">TECH</span></span></div>
         </div>
-        <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 justify-content-center mt-4 align-items-center">
-          <div className="col-12 col-sm-12 col-md-12 col-lg-11 col-xl-10 col-xxl-10 text-center align-items-center justify-content-center d-flex">
+        <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 justify-content-center align-items-center">
+          <div className="point-content col-12 col-sm-12 col-md-12 col-lg-11 col-xl-10 col-xxl-10">
             <div className="points" data-aos="fade-left">
-              Spend with the Crypto and get up to <span style={{ marginLeft: '5px', marginRight: '35px' }}>12% back</span>Invite friends and get <span style={{ marginLeft: '5px' }}>new opportunities</span>
+              Spend with the Crypto and get up to <span >12% back</span> Invite friends and get <span>new opportunities</span>
             </div>
           </div>
         </div>
-        <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 justify-content-center mt-3 align-items-center">
-          <div className="col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 col-xxl-10 text-center align-items-center justify-content-center d-flex">
+        <div className="row col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12 justify-content-center align-items-center">
+          <div className="point-content col-10 col-sm-10 col-md-10 col-lg-10 col-xl-10 col-xxl-10">
             <div className="points" data-aos="fade-right">
-              Trade and earn <span style={{ marginLeft: '5px' }}>200+ cryptocurrencies</span> and get a permanent <span style={{ marginLeft: '5px' }}>5% discount</span>
+              Trade and earn <span>200+ cryptocurrencies</span> and get a permanent <span>5% discount</span>
             </div>
           </div>
         </div>
